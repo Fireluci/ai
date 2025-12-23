2fac496249475f518ef1e0092f862601fc1cd18d
Fireluci <120563797+Fireluci@users.noreply.github.com>
2025-12-23 11:10:11 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,131 @@
+class script(object):
+    START_TXT = "<b>🌀 Unlimited Movies, Series, Anime\n🔆 New Releases Upload Same Day\n♻️ 24x7 Service 📆 Daily Updates</b>"
+ 
+    
+    HELP_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    PREMIUM_TXT = """<b>🌟 PREMIUM MEMBERSHiP 🌟
+〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
+🔆 Get All Movies + Series Direct Files Without Any Links or Ads!
+
+🗳 1 Month Plan ➔ ₹40/- 
+🗳 1 Year Plan ➔ ₹400/-
+
+🌀 To Buy ➔ Text @HeroFeedBot</b>"""
+    ABOUT_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+
+    SOURCE_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    MANUELFILTER_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+
+    BUTTON_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+
+    AUTOFILTER_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+
+    CONNECTION_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    EXTRAMOD_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    ADMIN_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+
+    STATUS_TXT = """<b>★ Tᴏᴛᴀʟ Fɪʟᴇs: <code>{}</code>
+★ Tᴏᴛᴀʟ Usᴇʀs: <code>{}</code>
+★ Tᴏᴛᴀʟ Cʜᴀᴛs: <code>{}</code>
+★ Usᴇᴅ Sᴛᴏʀᴀɢᴇ: <code>{}</code>
+★ Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ: <code>{}</code></b>"""
+
+    LOG_TEXT_G = """#NewGroup
+Gʀᴏᴜᴘ = {}(<code>{}</code>)
+Tᴏᴛᴀʟ Mᴇᴍʙᴇʀs = <code>{}</code>
+Aᴅᴅᴇᴅ Bʏ - {}"""
+
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Nᴀᴍᴇ - {}"""
+
+    ALRT_TXT = """🔆 Honey, It's Not For You❗
+🔆 हनी, ये तुम्हारे लिए नहीं है❗"""
+
+    OLD_ALRT_TXT = """♻ Link Expired, Please Request in Group Again!"""
+
+    CUDNT_FND = """<b>❗Enter Correct Name👇</b>
+<b>❗सही नाम दर्ज करें👇</b>"""
+
+    I_CUDNT = """<b><i>💢 No Results For Your Search❗️
+
+⚠️ The Reason❓[<a href="https://telegram.me/HeroFlixx/3">Click Here</a>]
+🌀 Please Follow Request Tips!
+🔆 Request Tips ➔ [<a href="https://telegram.me/HeroFlixx/2">Click Here</a>]</i></b>"""
+
+
+    I_CUD_NT = """<b><i>💢 No Results For Your Search❗️
+
+⚠️ The Reason❓[<a href="https://telegram.me/HeroFlixx/3">Click Here</a>]
+🔆 Please Follow Request Tips!
+🌀 Request Tips ➔ [<a href="https://telegram.me/HeroFlixx/2">Click Here</a>]</i></b>"""
+
+    MVE_NT_FND = """<b><i>💢 No Results For Your Search❗️
+
+⚠️ The Reason❓[<a href="https://telegram.me/HeroFlixx/3">Click Here</a>]
+🗨 Please Follow Request Tips!
+🔆 Request Tips ➔ [<a href="https://telegram.me/HeroFlixx/2">Click Here</a>]</i></b>"""
+
+    TOP_ALRT_MSG = """🔆彡[ @HEROFLiX ]彡🔆"""
+
+    MELCOW_ENG = """<b><i>🔆 "HEROFLiX • GROUP" 亗 🔆
+•───────────────────• 
+⚜Hey {}, Welcome To HEROFlix • Group. You Can Request Any Movies, Web-Series, Anime, K-Dramas, Animation etc., here....</i></b>"""
+
+    SHORTLINK_INFO = """🔆彡[ @HEROFLiX ]彡🔆"""
+
+    REQINFO = """🔆彡[ @HEROFLiX ]彡🔆"""
+
+    SINFO = """🔆彡[ @HEROFLiX ]彡🔆"""
+
+    NORSLTS = """
+★ #𝗡𝗼𝗥𝗲𝘀𝘂𝗹𝘁𝘀 ★
+
+𝗜𝗗 <b>: {}</b>
+
+𝗡𝗮𝗺𝗲 <b>: {}</b>
+
+𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
+
+    CAPTION = """
+ <a href="https://telegram.me/HEROFLiX"><b><i>{file_name}</i></b></a>"""
+
+    IMDB_TEMPLATE_TXT = """
+<b>Query: {query}
+IMDb Data:
+
+🏷 Title: <a href={url}>{title}</a>
+🎭 Genres: {genres}
+📆 Year: <a href={url}/releaseinfo>{year}</a>
+⏱️ Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
+🌟 Rating: <a href={url}/ratings>{rating}</a> / 10</b>"""
+    
+    ALL_FILTERS = """🔆彡[ @HEROFLiX ]彡🔆"""
+    
+    GFILTER_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    
+    FILE_STORE_TXT = """🔆彡[ @HEROFLiX ]彡🔆"""
+    RESTART_TXT = """
+<b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
+
+📅 Dᴀᴛᴇ : <code>{}</code>
+⏰ Tɪᴍᴇ : <code>{}</code>
+🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>Asia/Kolkata</code>
+🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code>v2.7.1 [ Sᴛᴀʙʟᴇ ]</code></b>"""
+
+    LOGO = """🔆彡[ HEROFLiX ]彡🔆"""
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
